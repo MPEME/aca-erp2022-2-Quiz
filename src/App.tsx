a167b6501533bc3ca96e55b66c547c46d089cb40
Mpeme <mpeme.ntsene@bothouniversity.com>
2023-08-08 16:42:15 +0200

    removed default template

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} style={{maxWidth:150, maxHeight:150}} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://africacode.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I Love ACA
-        </a>
-      </header>
+      <div>Hello Quiz</div>
     </div>
   );
 }
